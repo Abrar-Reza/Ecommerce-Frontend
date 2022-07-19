bf4308e5495a37b9b03f3d857dc37434cb05bdde
Abrar Reza <55370306+Abrar-Reza@users.noreply.github.com>
2022-07-18 21:19:07 -0400

    vue ts

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:1337/api/'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
